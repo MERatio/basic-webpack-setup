1cba862b3e08ee1b86dc74dfdb2f9ea0d027fd99
Mark Edward Ratio <markedwardratio@gmail.com>
2020-11-04 14:28:56 +0800

    Update setup

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import './style.css';
 import WebpackImg from './webpack.png';
 
 function component() {
-  const div = document.createElement('div');
+	const div = document.createElement('div');
 
-  const p = document.createElement('p');
-  p.textContent = 'Webpack Test';
-  div.appendChild(p);
+	const p = document.createElement('p');
+	p.textContent = 'Webpack Test';
+	div.appendChild(p);
 
-  const img = new Image();
-  img.src = WebpackImg;
-  div.appendChild(img);
+	const img = new Image();
+	img.src = WebpackImg;
+	div.appendChild(img);
 
-  return div;
+	return div;
 }
 
 document.body.appendChild(component());
